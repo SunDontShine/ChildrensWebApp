d9fd63db5609b52509ffe0bed06a2e093f793889
SunDontShine <davila.carlos75@yahoo.com>
2016-12-17 17:37:22 -0700

    fixed url issue, changed js file name

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', function(req,res){
 //contains views/*.html
 
 //request including basic 'home template' layout
-app.get(/\/home/,function(req,res){
+app.get(/\/main/,function(req,res){
 
      var reqPath = url.parse(req.url).pathname;
     console.log(reqPath);
@@ -31,9 +31,9 @@ app.get(/\/home/,function(req,res){
     
 });
 
-app.get(/\/main.js/,function(req,res){
+app.get(/\/Model\/functions.js/,function(req,res){
     var reqPath = url.parse(req.url).pathname;
-    res.sendFile(__dirname+'/Model/main.js');
+    res.sendFile(__dirname+'/Model/functions.js');
 })
 app.get(/\/Views\/games.html/,function(req,res){
     res.sendFile(__dirname+'/Views/games.html');
