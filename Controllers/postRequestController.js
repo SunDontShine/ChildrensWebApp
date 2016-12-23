54ba1b9fc78bcc8819f439dd48ec291242c73801
SunDontShine <davila.carlos75@yahoo.com>
2016-12-23 01:26:33 -0700

    complete signup page layout

---
+++ Controllers/postRequestController.js
@@ -0,0 +1,10 @@
+var fs = require('fs')
+var path = require('path')
+
+module.export {
+    
+    handlePostRequest : function(req,res){
+        
+    }
+    
+}
--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var app = express();
 var url = require('url')
 var views="/Views/"
 var vc = require('./Controllers/viewController.js');
-
+var prc = require('./Controllers/postRequestController.js');
+var bparser = require('body-parser');
+var qs = require('querystring');
 app.get('/', function(req,res){
     res.redirect(302,'/main/login')
 })
@@ -17,7 +19,7 @@ app.get('/', function(req,res){
 app.get(/\/main/,function(req,res){
 
      var reqPath = url.parse(req.url).pathname;
-    console.log(reqPath);
+    //console.log(reqPath);
     var reqFile = path.basename(reqPath);
     fs.readdir(__dirname+views,'utf8',function(err,data){
         if(data.includes(reqFile+'.html')){
@@ -41,7 +43,7 @@ app.get(/\/Views\/subj\//,function(req,res){
 })
 app.get(/\/Views\/styles\//,function(req,res){
     var reqPath = url.parse(req.url).pathname;
-    console.log(reqPath);
+    //console.log(reqPath);
     var reqFile = path.basename(reqPath); // the requested file
     
     fs.readdir(__dirname+views+'styles','utf8',function(err,data){
@@ -55,5 +57,19 @@ app.listen(3002, function(){
     console.log("Example App Listening on port 3002!");
 });
 
-
+app.post(/\/main\//,function(req,res){
+    var reqPath = path.basename(req.path);
+    console.log(reqPath);
+    
+    var body="";
+    req.on('data',function(data){
+               body+=data;
+    });
+    req.on('end',function(){
+        var postData = qs.parse(body);
+        console.log(postData);
+        //prc.validateUser();
+    })
+    
+});
 
